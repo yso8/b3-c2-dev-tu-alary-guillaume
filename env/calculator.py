13c6d548c97821bca3c4429b840156013c80e710
yso <baptisteguillaume5@gmail.com>
2023-03-16 21:09:22 +0100

    percentage and root-squared

--- env/calculator.py
+++ env/calculator.py
@@ -1,7 +1,11 @@
+import math
+
 class Calculator:
     def calculate(expression):
+        # Supprimer les espaces en trop de l'expression
         expression = expression.replace(" ", "")
 
+        # Convertir l'expression en une liste de tokens (nombres et opérateurs)
         tokens = []
         current_token = ""
 
@@ -17,9 +21,10 @@ class Calculator:
         if current_token:
             tokens.append(current_token)
         
+        # Évaluer l'expression 
         output_queue = []
         operator_stack = []
-        operator_precedence = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3}
+        operator_precedence = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3, "%": 2, "√": 4}
 
         for token in tokens:
             if token.isdigit() or "." in token:
@@ -38,10 +43,20 @@ class Calculator:
         while operator_stack:
             output_queue.append(operator_stack.pop())
 
+        # Calculer le résultat de l'expression à partir de la file de sortie
         operand_stack = []
         for token in output_queue:
             if isinstance(token, float):
                 operand_stack.append(token)
+            elif token == "%":
+                operand1 = operand_stack.pop()
+                operand2 = operand_stack.pop()
+                result = operand1 * operand2 * 0.01
+                operand_stack.append(result)
+            elif token == "√":
+                operand1 = operand_stack.pop()
+                result = math.sqrt(operand1)
+                operand_stack.append(result)
             elif token in operator_precedence:
                 operand2 = operand_stack.pop()
                 operand1 = operand_stack.pop()
